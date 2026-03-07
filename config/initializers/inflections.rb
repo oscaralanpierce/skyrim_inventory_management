9b0fa2793a1f6eaa78c6c09912f4a0b127cb89ab
Dana Scheider <dana.scheider@gmail.com>
2026-03-08 09:50:24 +1100

    Make a couple changes after code review

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,13 +8,13 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, "\\1en"
 #   inflect.singular /^(ox)en/i, "\\1"
-#   inflect.irregular "person", "people"
+#   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
+#   inflect.acronym 'RESTful'
 # end
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
