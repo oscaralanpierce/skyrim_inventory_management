d8b24ee88347bc528fd88d884af9eec8c31300d9
Dana Scheider <dana.scheider@gmail.com>
2026-03-08 12:07:46 +1100

    Fix a couple things after reviewing code

--- config/puma.rb
+++ config/puma.rb
@@ -30,6 +30,9 @@
 threads_count = ENV.fetch('RAILS_MAX_THREADS', 3)
 threads threads_count, threads_count
 
+# Specifies the Rails `environment`
+environment ENV.fetch('RAILS_ENV', 'development')
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch('PORT', 3000)
 
