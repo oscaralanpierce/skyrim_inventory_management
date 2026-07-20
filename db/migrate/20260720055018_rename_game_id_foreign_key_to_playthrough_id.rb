028cfdf4da5c45adfb92bab15c2a7bc39685da93
Oscar Alan Pierce <oscar.alan.pierce@gmail.com>
2026-07-20 18:10:25 +1000

    Fix migration that had been messed up with find-and-replace

--- db/migrate/20260720055018_rename_game_id_foreign_key_to_playthrough_id.rb
+++ db/migrate/20260720055018_rename_game_id_foreign_key_to_playthrough_id.rb
@@ -2,17 +2,17 @@
 
 class RenameGameIdForeignKeyToPlaythroughId < ActiveRecord::Migration[8.1]
   def change
-    rename_column :armors, :playthrough_id, :playthrough_id
-    rename_column :books, :playthrough_id, :playthrough_id
-    rename_column :clothing_items, :playthrough_id, :playthrough_id
-    rename_column :ingredients, :playthrough_id, :playthrough_id
-    rename_column :inventory_lists, :playthrough_id, :playthrough_id
-    rename_column :jewelry_items, :playthrough_id, :playthrough_id
-    rename_column :misc_items, :playthrough_id, :playthrough_id
-    rename_column :potions, :playthrough_id, :playthrough_id
-    rename_column :properties, :playthrough_id, :playthrough_id
-    rename_column :staves, :playthrough_id, :playthrough_id
-    rename_column :weapons, :playthrough_id, :playthrough_id
-    rename_column :wish_lists, :playthrough_id, :playthrough_id
+    rename_column :armors, :game_id, :playthrough_id
+    rename_column :books, :game_id, :playthrough_id
+    rename_column :clothing_items, :game_id, :playthrough_id
+    rename_column :ingredients, :game_id, :playthrough_id
+    rename_column :inventory_lists, :game_id, :playthrough_id
+    rename_column :jewelry_items, :game_id, :playthrough_id
+    rename_column :misc_items, :game_id, :playthrough_id
+    rename_column :potions, :game_id, :playthrough_id
+    rename_column :properties, :game_id, :playthrough_id
+    rename_column :staves, :game_id, :playthrough_id
+    rename_column :weapons, :game_id, :playthrough_id
+    rename_column :wish_lists, :game_id, :playthrough_id
   end
 end
